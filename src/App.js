743c922e9d04e118bb809d99757fa5b62bcae712
nlom0218 <nlom0218@naver.com>
2021-11-13 16:03:40 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 function App() {
-  useEffect(() => {
-    window.alert("안녕!")
-  })
   return (
     <div className="App">
-      <div>음식</div>
-      <ul>
-        <li>햄버거</li>
-        <li>핫도그</li>
-        <li>피자</li>
-        <li>치킨</li>
-        <li>짜장면</li>
-        <li>라면</li>
-        <li>떡볶이</li>
-      </ul>
+
     </div>
   );
 }
