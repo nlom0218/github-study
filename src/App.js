6994bb83d03abaf7b4dd92b25fdfd3135acded07
nlom0218 <nlom0218@naver.com>
2021-11-14 22:05:54 +0900

    Hello #1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import { Link, Route, Router, Routes } from 'react-router-dom';
-import styled from 'styled-components';
-import Header from './Components/Header';
+import { Route, Routes } from 'react-router-dom';
 import Kimoo from './Pages/1';
+import Hello from './Pages/5';
 import Home from './Pages/Home';
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-`
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/1" element={<Kimoo />} />
+      <Route path="/5" element={<Hello />} />
     </Routes>
   );
 }
--- src/Pages/5.js
+++ src/Pages/5.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from '../Components/Header';
+
+const Container = styled.div`
+  font-size: 24px;
+  color: tomato;
+`
 
 const Hello = () => {
-  return (<div>Hello</div>);
+  const onClick = () => {
+    window.alert("반갑습니다!")
+  }
+  return (
+    <React.Fragment>
+      <Header />
+      <Container onClick={onClick}>안녕하세요</Container>
+    </React.Fragment>
+  );
 }
 
 export default Hello;
