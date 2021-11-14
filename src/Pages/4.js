56db661783d1d18885bee32c592659137b21518c
nlom0218 <nlom0218@naver.com>
2021-11-14 23:28:59 +0900

    33

--- src/Pages/4.js
+++ src/Pages/4.js
@@ -2,62 +2,62 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div
-    font-size24px;
-    color:tomato;
+font - size24px;
+color: tomato;
 
 
 const hi = () => {
 
-const onClick = () => { 
-    window.alert;{"안녕"}
-}
+    const onClick = () => {
+        window.alert; { "안녕" }
+    }
 
-    return ( 
+    return (
 
-<React.Fragment>
-<Header/>
-<Container onClick={onClick}>안녕하세요</Container>
-<h1>butterfly</h1>
+        <React.Fragment>
+            <Header />
+            <Container onClick={onClick}>안녕하세요</Container>
+            <h1>butterfly</h1>
 
-<style>
-        width: 100vw;
-        height: 100vh;
-        background: linear-gradient(
-            to right,
-            rgba(20, 20, 20, 0) 10%,
-            rgba(20, 20, 20, 0.25) 25%,
-            rgba(20, 20, 20, 0.5) 50%,
-            rgba(20, 20, 20, 0.75) 75%,
-            rgba(20, 20, 20, 1) 100%
-          ), url(https://source.unsplash.com/random);
-        background-size: cover;
+            <style>
+                width: 100vw;
+                height: 100vh;
+                background: linear-gradient(
+                to right,
+                rgba(20, 20, 20, 0) 10%,
+                rgba(20, 20, 20, 0.25) 25%,
+                rgba(20, 20, 20, 0.5) 50%,
+                rgba(20, 20, 20, 0.75) 75%,
+                rgba(20, 20, 20, 1) 100%
+                ), url(https://source.unsplash.com/random);
+                background-size: cover;
 
     </style>
 
-<div id="intro">
-    <h2>About butterfly </h2>
-    <p>
-        <strong>"Butterfly"</strong>
-    <br></br>
+            <div id="intro">
+                <h2>About butterfly </h2>
+                <p>
+                    <strong>"Butterfly"</strong>
+                    <br></br>
     "fly fly fly"
     <ul>
-    <li>"one"</li>
-    <li>"two"</li>
-    <li>"three"</li>
-    </ul>
-    </p>
-    <button className="square" onClick={() => console.log('click')}>
-       {this.props.value}
-     </button>
-     </div>
-<div id="media">
-    <h2>photo</h2>
-    <a href="https://google.com">
-    <img src="https://www.nemopan.com/files/attach/images/2580/148385/48255eca6c174.jpg" alt="butterfly"></img>
-    </a>
-</div>
-</React.Fragment>
-);
+                        <li>"one"</li>
+                        <li>"two"</li>
+                        <li>"three"</li>
+                    </ul>
+                </p>
+                <button className="square" onClick={() => console.log('click')}>
+                    {this.props.value}
+                </button>
+            </div>
+            <div id="media">
+                <h2>photo</h2>
+                <a href="https://google.com">
+                    <img src="https://www.nemopan.com/files/attach/images/2580/148385/48255eca6c174.jpg" alt="butterfly"></img>
+                </a>
+            </div>
+        </React.Fragment>
+    );
 }
- 
+
 export default hi;
