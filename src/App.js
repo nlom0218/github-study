2bfa9b4972b467a8cae21b861e5efbfaa84de825
soave424 <89090628+soave424@users.noreply.github.com>
2021-11-17 23:11:10 +0900

    commit info

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Kimoo from './Pages/1';
 import Hello from './Pages/5';
 import Home from './Pages/Home';
 import Three from './Pages/3';
+import Four from './Pages/4';
+
 
 
 function App() {
@@ -12,7 +14,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/1" element={<Kimoo />} />
       <Route path="/3" element={<Three />} />
-      <Route path="/4" element={<hi />} />
+      <Route path="/4" element={<Four />} />
       <Route path="/5" element={<Hello />} />
     </Routes>
   );
--- src/Pages/4.js
+++ src/Pages/4.js
@@ -1,63 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import Header from '../Components/Header';
 
-const Container = styled.div
-font - size24px;
-color: tomato;
+const BasicContainer = styled.div`
+    display:grid;
+    grid-template-columns: 2fr 1fr 3fr;
+    justify-items: center;
+    `
+const Container =styled.div`
+    font-size: 50px;
+    grid-column:2/4;
+    row-gap:1/3;
+    background-color:tomato;
+    justify-self : start;`
 
+const Btn = styled.div`
+    width: 1cm;
+    height: 1cm;
+    font-size: 30px;
+    background-color: lightblue;
+    cursor: pointer;
+    justify-self:center;
+    `
 
-const hi = () => {
+const Four = () => {
 
-    const onClick = () => {
-        window.alert; { "안녕" }
+const [num,setNum] = useState(0)
+
+const onClickBtn= (mode)=> {
+    if (mode === "plus") {
+        setNum(prev => prev +1)
+    } else if (mode === "minus") {
+        setNum(prev => prev -1)
+    }else if (mode === "reset"){
+        setNum(0)
     }
-
-    return (
-
-        <React.Fragment>
-            <Header />
-            <Container onClick={onClick}>안녕하세요</Container>
-            <h1>butterfly</h1>
-
-            <style>
-                width: 100vw;
-                height: 100vh;
-                background: linear-gradient(
-                to right,
-                rgba(20, 20, 20, 0) 10%,
-                rgba(20, 20, 20, 0.25) 25%,
-                rgba(20, 20, 20, 0.5) 50%,
-                rgba(20, 20, 20, 0.75) 75%,
-                rgba(20, 20, 20, 1) 100%
-                ), url(https://source.unsplash.com/random);
-                background-size: cover;
-
-    </style>
-
-            <div id="intro">
-                <h2>About butterfly </h2>
-                <p>
-                    <strong>"Butterfly"</strong>
-                    <br></br>
-    "fly fly fly"
-    <ul>
-                        <li>"one"</li>
-                        <li>"two"</li>
-                        <li>"three"</li>
-                    </ul>
-                </p>
-                <button className="square" onClick={() => console.log('click')}>
-                    {this.props.value}
-                </button>
-            </div>
-            <div id="media">
-                <h2>photo</h2>
-                <a href="https://google.com">
-                    <img src="https://www.nemopan.com/files/attach/images/2580/148385/48255eca6c174.jpg" alt="butterfly"></img>
-                </a>
-            </div>
-        </React.Fragment>
-    );
 }
 
-export default hi;
+    return ( 
+    <BasicContainer>
+    <Header />
+    <Container>
+    {num}
+        </Container>
+        <div>
+            <Btn onClick={()=>onClickBtn("plus")}>+</Btn>
+            <Btn onClick={()=>onClickBtn("minus")}>-</Btn>
+            <Btn onClick={()=>onClickBtn("reset")}>0</Btn>
+        </div>
+    </BasicContainer>
+     );
+
+    }
+export default Four;
