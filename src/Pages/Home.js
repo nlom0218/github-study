2ebf9f5bd43134c9f20d3b171a3c01808e3f82ff
nlom0218 <nlom0218@naver.com>
2021-11-17 23:09:30 +0900

    change title

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Header from '../Components/Header';
+import Hello from './5';
 
 const Container = styled.div`
   display: flex;
@@ -17,6 +18,7 @@ const Home = () => {
       <Link to="/3">티코 선생님</Link>
       <Link to="/4">조은 선생님</Link>
       <Link to="/5">크리링 선생님</Link>
+      <Hello />
     </ Container>
   );
 }
