0ad2c9eae801017053bcfa5d3dc9ab2def1253a1
SHJcompany <sksthsaudgml@gmail.com>
2021-11-17 23:12:39 +0900

    page 2 add

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Kimoo from './Pages/1';
 import Hello from './Pages/5';
 import Home from './Pages/Home';
 import Three from './Pages/3';
-
+import SHJ from './Pages/2';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/1" element={<Kimoo />} />
+      <Route path="/2" element={<SHJ />} />
       <Route path="/3" element={<Three />} />
       <Route path="/4" element={<hi />} />
       <Route path="/5" element={<Hello />} />
