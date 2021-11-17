407bbb0324e3e06edf7cdbdaf6888c1d3df72b3a
teacher-kiwi <kwi07@hanmail.net>
2021-11-17 22:33:22 +0900

    1.js 날씨 기능 추가

---
+++ src/Components/Weather.js
@@ -0,0 +1,27 @@
+import React from "react";
+import axios from "axios";
+
+class Weather extends React.Component {
+  state = {
+    weather: "loading...",
+  };
+
+  getWeather = async () => {
+    const {
+      data: {
+        weather: [{ main: data }],
+      },
+    } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=284ec2b148a72a61795ba16c7a6f7fbb&units=metric`
+    );
+    this.setState({ weather: data });
+  };
+
+  render() {
+    this.getWeather();
+    const { weather } = this.state;
+    return <h1>서울의 날씨는 : {weather}</h1>;
+  }
+}
+
+export default Weather;
